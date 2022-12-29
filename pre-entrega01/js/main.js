bda12c84a1437ad941eef2aa547ad3fc5f048eb5
MateoCoriaChris <106635887+MateoCoriaChris@users.noreply.github.com>
2022-12-29 18:03:17 -0300

    Update main.js

--- pre-entrega01/js/main.js
+++ pre-entrega01/js/main.js
@@ -28,52 +28,70 @@ let productoIngresadoB
 let productoIngresadoC
 
 do {producto=prompt('¿Que se les ofrece?');
-    productoNoValido=producto!==productoIngresadoA||productoIngresadoB||productoIngresadoC
+    productoNoValido=producto!=='1'||'2'||'3'
+    switch(producto){
+      case '1':
+            productoIngresadoA=prompt('¿Que clase de helado va a querer?');
+            switch(productoIngresadoA){
+                  case '10':
+                  cucuruchoSimple=alert('el cucurucho simple sale 300$¿Que sabores va a querer en su cucurucho simple?');
+                  Gusto();
+                  break;
+                  case '11':
+                        cucuruchoDoble=alert('el cucrucho doble sale 380$¿Que sabores va a querer en su cucurucho doble?')
+                        Gusto();
+                        break;
+                  case '12':
+                        heladoDeUnCuarto=alert('el helado de 1/4kg sale 420$¿Que sabores va a querer en su Helado de 1/4?')
+                        Gusto();
+                        break;
+                  case '13':
+                        heladoDeMedio=alert('el helado de 1/2kg sale 780$¿Que sabores va a querer en su Helado de 1/2kg?')
+                        Gusto();
+                        break;
+                  case '14':
+                        heladoDeUnKilo=prompt('el helado de 1kg sale 1260$¿Que sabores va a querer en su helado de 1kg?')
+                        Gusto();
+                        break;
+                  default:
+                        alert('El helado ingresado no existe. Ingrese otro numero entre 10 y 14');
+            }
+            break;
+      case '2':
+            productoIngresadoB=prompt('¿Que clase de pastel va a querer?');
+            switch(productoIngresadoB){
+                  case'20':
+                  alert('el pastel cheesecake sale 2500$');
+                  break;
+                  case'21':
+                  alert('el pastel selva negra sale 2550$');
+                  break;
+                  case'22':
+                  alert('el pastel de frutilla sale 2450$');
+                  break;
+                  default:
+                        alert('El pastel ingresado no existe. Ingrese otro numero entre 20 y 23');   
+            }
 
-    if(producto = 1){
-         productoIngresadoA=prompt('¿Que clase de helado va a querer?');
-         if(productoIngresadoA == 10){
-            cucuruchoSimple=alert('Que sabores va a querer en su cucurucho simple?');
-            Gusto();
-         }else if(productoIngresadoA == 11){
-            cucuruchoDoble=alert('¿Que sabores va a querer en su cucurucho doble?')
-            Gusto();
-         }else if (productoIngresadoA == 12){
-            heladoDeUnCuarto=alert('¿Que sabores va a querer en su Helado de 1/4?')
-            Gusto();
-         }else if (productoIngresadoA == 13){
-            heladoDeMedio=alert('¿Que sabores va a querer en su Helado de 1/2kg?')
-            Gusto();
-         }else if (productoIngresadoA == 14){
-            heladoDeUnKilo=prompt('¿Que sabores va a querer en su?')
-            Gusto();
-         }else{alert('El helado ingresado no existe. Ingrese otro numero entre 10 y 14')
-      }         
-         break;
-
-   }else if(producto = 2){
-         productoIngresadoB=prompt('¿Que clase de pastel va a querer?');
-         if(productoIngresadoB == 20){
-             alert('el pastel cheesecake sale 2500$')
-         }else if (productoIngresadoB == 21){
-             alert('el pastel selva negra sale 2550$')
-         }else if (productoIngresadoB == 22){
-             alert('el pastel de frutilla sale 2450$')
-         }else{alert('El pastel ingresado no existe. Ingrese otro numero entre 20 y 23')
-       }
-         break;
-
-   }else if(producto = 3){
-         productoIngresadoC=prompt('¿Que clase de helado de agua va a querer?')
-         if(productoIngresadoC == 31){
-            alert('el helado de agua de naranja sale 200$')
-         }else if(productoIngresadoC == 32){
-            alert('el helado de agua de limon sale 200$')
-         }else if(productoIngresadoC == 33){
-            alert('el helado de agua de frutilla sale 200$')
-         }else{alert('El helado de agua ingresado no existe. Ingrese otro numero entre 30 y 33')}
-         break;
-
-   }else{alert('El producto ingresado no existe. Ingrese otro numero entre 1 y 3')
-   }
+            break;
+      case '3':
+            prompt('¿Que clase de helado de agua va a querer?');
+            productoIngresadoC=prompt('¿Que clase de helado de agua va a querer?');
+            switch(productoIngresadoC){
+                  case'30':
+                  alert('el helado de agua de naranja sale 200$');
+                  break;
+                  case'31':
+                  alert('el helado de agua de limon sale 200$');
+                  break;
+                  case'32':
+                  alert('el helado de agua de frutilla sale 200$');
+                  break;
+                  default:
+                        alert('El helado de agua ingresado no existe. Ingrese otro numero entre 30 y 32');
+            }
+            break;
+      default:
+            alert('El producto ingresado no existe. Ingrese otro numero entre 1 y 3');   
+    }
 }while(producto===productoNoValido);
